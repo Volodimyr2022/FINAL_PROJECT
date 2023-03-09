605220dd382579f81a3adee9f0bfa9f293a95a1c
Vladimir <107769314+Volodimyr2022@users.noreply.github.com>
2023-03-09 05:41:31 +0200

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,106 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/styles/style.css":
+/*!******************************!*\
+  !*** ./src/styles/style.css ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://final-project/./src/styles/style.css?");
+
+/***/ }),
+
+/***/ "./src/components/API.js":
+/*!*******************************!*\
+  !*** ./src/components/API.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"TOKEN_KEY\": () => (/* binding */ TOKEN_KEY),\n/* harmony export */   \"api\": () => (/* binding */ api)\n/* harmony export */ });\nconst TOKEN_KEY = \"token\";\r\n\r\nclass API {\r\n    constructor() {\r\n        this.baseUrl = \"https://byte-tasks.herokuapp.com/api\";\r\n        this.headers = {\r\n            Authorization: null,\r\n            \"Content-Type\": \"application/json\",\r\n        };\r\n    }\r\n\r\n    async register(data) {\r\n        const response = await fetch(`${this.baseUrl}/auth/register`, {\r\n            method: \"POST\",\r\n            headers: this.headers,\r\n            body: JSON.stringify(data),\r\n        });\r\n    \r\n        const registeredUser = await response.json();\r\n    \r\n       return registeredUser;\r\n    }\r\n    \r\n\r\nasync login(data) {\r\n    const response = await fetch(`${this.baseUrl}/auth/login`, {\r\n        method: \"POST\",\r\n        headers: this.headers,\r\n        body: JSON.stringify(data),\r\n    });\r\n\r\n    const { token } = await response.json();\r\n\r\n    this.headers.Authorization = `Bearer ${token}`;\r\n    localStorage.setItem(TOKEN_KEY, token);\r\n}\r\n\r\nasync getSelf() {\r\n    const res = await fetch(`${this.baseUrl}/auth/user/self`, {\r\n        method: \"GET\",\r\n        headers: this.headers,\r\n    });\r\n   \r\n    return res;\r\n}\r\n\r\n}\r\n\r\n\r\nconst api = new API();\n\n//# sourceURL=webpack://final-project/./src/components/API.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/style.css */ \"./src/styles/style.css\");\n/* harmony import */ var _components_API_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/API.js */ \"./src/components/API.js\");\n\r\n\r\n\r\n\r\n// api.login({\r\n//     email: 'vova@gmail.com',\r\n//     password: 'vova',\r\n//  }).then((res) => {\r\n//     console.log(res)\r\n//     api.getSelf();\r\n// }).catch((err) => {\r\n//     console.log(err)\r\n// });\r\n\r\n\r\n_components_API_js__WEBPACK_IMPORTED_MODULE_1__.api.register({\r\n    email: 'fff@gmail.com',\r\n    password: 'fff'\r\n})\n\n//# sourceURL=webpack://final-project/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
